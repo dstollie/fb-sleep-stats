66126b5d54efdf34915509ce52fa9a290b63ef60
Dennis Stolmeijer <dstollie@gmail.com>
2017-09-11 07:12:51 +0200

    - Added simple bash script - Added mongo as dependency

--- src/server/services/dao.js
+++ src/server/services/dao.js
@@ -3,12 +3,25 @@ var path = require('path');
 var lowdb = require('lowdb');
 var storage = require('lowdb/file-async');
 var dao = {};
-var REFRESH_RATE = 1000 * 60 * 10;
+var REFRESH_RATE = 1000 * 30;
 var dbPath = path.resolve(__dirname, 'db.json');
 var db = getDb();
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/fb-sleep-stats';
 
 if (!isDbValid(db)) {
     createDb();
+
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+
+        db.close();
+    });
 }
 
 function getDb() {
